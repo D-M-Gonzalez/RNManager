17e307a33419c991b0f264e7d9f8c555f505ff2e
Sifflion <dami.m.gonza@gmail.com>
2022-04-30 17:11:48 -0300

    production

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
-import express from 'express';
-import path from 'path';
+const path = require('path');
+const express = require('express');
+
+const app = express();
 
 const publicPath = path.join(__dirname, '..', 'public');
-const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(publicPath,'index.html'));
+})
 
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}!`);
- });
+app.listen(port, ()=> {
+    console.log('server is up!');
+})
